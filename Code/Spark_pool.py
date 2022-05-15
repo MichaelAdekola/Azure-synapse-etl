fa9f6ea5594819a68d3c6e6bab5419909e0d7b3f
MichaelAdekola <michaelscott658@gmil.com>
2022-05-15 20:58:20 +1000

    initial commit

---
+++ Code/Spark_pool.py
@@ -0,0 +1,26 @@
+## Spark pool
+
+df = spark.read.load('abfss://data@bigmike_storage1.dfs.core.windows.net/NYCTripSmall.parquet', format='parquet') #Please don't forget to replace your storage and file system name
+df.printSchema()
+display(df.limit(10))
+
+# Replace storage name with yours
+
+spark.sql("CREATE DATABASE IF NOT EXISTS nyctaxi")
+df.repartition(1)
+df.write.mode("overwrite").saveAsTable("nyctaxi.trip")
+spark.sql("Select count(*) from nyctaxi.trip")
+
+
+
+df = spark.sql("""
+   SELECT PassengerCount,
+       SUM(TripDistanceMiles) as SumTripDistance,
+       AVG(TripDistanceMiles) as AvgTripDistance
+   FROM nyctaxi.trip
+   WHERE TripDistanceMiles > 0 AND PassengerCount > 0
+   GROUP BY PassengerCount
+   ORDER BY PassengerCount
+""") 
+display(df)
+df.write.saveAsTable("nyctaxi.passengercountstats")
